5d65fa47945b9ad6f4f4f28fba41cf874bae169c
mikbill <helgi8@mail.ru>
2016-10-13 15:43:16 +0300

    Create radiuscash.sh

---
+++ radiuscash/radiuscash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+USERMAN_IP="192.168.10.150"
+USERMAN_LOGIN="mikbill"
+USERMAN_PASSWORD="mikbill"
+UPLOAD="userman.rsc"
+PATH_CONFIG=/var/www/mikbill/admin/app/etc/config.xml
+DB_USER=$(cat $PATH_CONFIG| grep  username | awk '{ gsub("<username>"," "); print }' | awk '{ gsub("</username>"," "); print }' | awk '{print $1}')
+DB_PASSWORD=$(cat $PATH_CONFIG| grep  password | awk '{ gsub("<password>"," "); print }' | awk '{ gsub("</password>"," "); print }' | awk '{print $1}')
+DB_NAME=$(cat $PATH_CONFIG | grep dbname |$AWK '{ gsub("<dbname>"," "); print }' | awk '{ gsub("</dbname>"," "); print }'| awk '{print $1}'`
+
+HOME_DIR=$(cd $(dirname $0)&& pwd)
+
+MAC=`mysql -D mikbill -u $DB_USER -p$DB_PASSWORD -e "SELECT local_mac FROM users" 2>/dev/null`
+echo "/tool user-manager user remove [find]" > $HOME_DIR/$UPLOAD
+for i in $MAC; do
+echo "/tool user-manager user add customer=admin name=$i" >>$HOME_DIR/$UPLOAD
+done
+
+curl --upload-file $HOME_DIR/$UPLOAD  ftp://$USERMAN_LOGIN:$USERMAN_PASSWORD@$USERMAN_IP/
+CMD="/import file=$UPLOAD"
+ssh $USERMAN_LOGIN@$USERMAN_IP "${CMD}" > /dev/null
