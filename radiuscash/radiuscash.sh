b7fe658d38fc68472c8b372ca2e304b0bc9864aa
mikbill <helgi8@mail.ru>
2016-10-14 12:41:50 +0300

    Update radiuscash.sh

--- radiuscash/radiuscash.sh
+++ radiuscash/radiuscash.sh
@@ -14,26 +14,42 @@ DB_NAME=$(cat $PATH_CONFIG | grep dbname | awk '{ gsub("<dbname>"," "); print }'
 
 HOME_DIR=$(cd $(dirname $0)&& pwd)
 
+echo "/tool user-manager user remove [find]" > $HOME_DIR/$UPLOAD
+
 case "$RADIUS_TYPE" in
 "hotspot") 
 INQUIRY="SELECT local_mac FROM users WHERE credit >= ABS (deposit) and blocked=0"
 MAC=`mysql -D $DB_NAME -u $DB_USER -p$DB_PASSWORD -e "$INQUIRY" 2>/dev/null`
 MAC=${MAC:10:${#MAC}}
+for i in $MAC; do
+echo "/tool user-manager user add customer=admin username=$i" >>$HOME_DIR/$UPLOAD
+done
+
+
 ;;
 "ppp")
 INQUIRY="SELECT user, password FROM users WHERE credit >= ABS (deposit) and blocked=0;"
 MAC=`mysql -D $DB_NAME -u $DB_USER -p$DB_PASSWORD -e "$INQUIRY" 2>/dev/null`
 MAC=${MAC:14:${#MAC}}
+
+NUM=0
+for i in $MAC; do
+LOGIN_PASS[$NUM]=$i
+let "NUM=NUM+1"
+done
+
+for((i=0;i!=NUM;i+=2))
+do
+echo "/tool user-manager user add customer=admin username=$LOGIN_PASS[$i] password=$LOGIN_PASS[$i+1]" >>$HOME_DIR/$UPLOAD
+done
+
 ;;
 esac
 
 echo "$MAC"
 
-echo "/tool user-manager user remove [find]" > $HOME_DIR/$UPLOAD
-for i in $MAC; do
-echo "/tool user-manager user add customer=admin username=$i" >>$HOME_DIR/$UPLOAD
-done
 echo "/tool user-manager user create-and-activate-profile profile=admin customer=admin numbers=[find]" >> $HOME_DIR/$UPLOAD
+
 
 
 curl --upload-file $HOME_DIR/$UPLOAD  ftp://$USERMAN_LOGIN:$USERMAN_PASSWORD@$USERMAN_IP/
