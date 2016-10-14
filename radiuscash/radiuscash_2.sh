766abe0f308168af3674f905c5918d173aa87ca9
mikbill <helgi8@mail.ru>
2016-10-15 01:42:55 +0300

    Update radiuscash_2.sh

--- radiuscash/radiuscash_2.sh
+++ radiuscash/radiuscash_2.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-USERMAN_IP="192.168.10.66"
-USERMAN_LOGIN="mikbill"
-USERMAN_PASSWORD="mikbill"
-
-#RADIUS_TYPE="hotspot"
-RADIUS_TYPE="ppp"
-
 HOME_DIR=$(cd $(dirname $0)&& pwd)
-UPLOAD=$HOME_DIR/userman.rsc
-LIST_NEW=$HOME_DIR/list.new
-LIST_OLD=$HOME_DIR/list.old
-CURL_TRUE=$HOME_DIR/curl.true
-SSH_TRUE=$HOME_DIR/ssh.true
+source $HOME_DIR/radiuscash_2.conf
 
-PATH_CONFIG=/var/www/mikbill/admin/app/etc/config.xml
-DB_USER=$(cat $PATH_CONFIG| grep  username | awk '{ gsub("<username>"," "); print }' | awk '{ gsub("</username>"," "); print }' | awk '{print $1}')
-DB_PASSWORD=$(cat $PATH_CONFIG| grep  password | awk '{ gsub("<password>"," "); print }' | awk '{ gsub("</password>"," "); print }' | awk '{print $1}')
-DB_NAME=$(cat $PATH_CONFIG | grep dbname | awk '{ gsub("<dbname>"," "); print }' | awk '{ gsub("</dbname>"," "); print }'| awk '{print $1}')
+FUNC_DATE (){
+DATE=`date +%Y-%m-%d_%Hh-%Mm-%Ss`
+}
+FUNC_DATE
+echo "$DATE start" >>$LOG
+if ! ([ -e $LOG_LINK ])
+then
+ln -s $LOG $LOG_LINK
+fi
 
-rm $UPLOAD
+rm $UPLOAD $CURL_TRUE $SSH_TRUE
 
 INQUIRY="SELECT MAX( uid ) FROM users"
 MAX=`mysql -D $DB_NAME -u $DB_USER -p$DB_PASSWORD -e "$INQUIRY" 2>/dev/null`
@@ -71,14 +65,47 @@ fi
 
 done
 
+
 fi
 echo "/tool user-manager user create-and-activate-profile profile=admin customer=admin numbers=[find]" >> $UPLOAD
 
-if ([ -e "$CURL_TRUE" && -e "$SSH_TRUE" ])
+i=$CONNECT_SUM
+while [ $i -ne 0 ]
+do
+curl --upload-file $UPLOAD  ftp://$USERMAN_LOGIN:$USERMAN_PASSWORD@$USERMAN_IP/
+STATUS=$?
+FUNC_DATE
+if [ $STATUS -ne 0 ]
 then
-#cp $LIST_NEW $LIST_OLD
+let i=i-1
+echo "$DATE curl no connect" >>$LOG
+sleep $CONNECT_INTERVAL
+else
+echo "$DATE curl connect OK" >>$LOG
+touch $CURL_TRUE
+i=0
 fi
+done
+
+i=$CONNECT_SUM
+CMD="/import file=$UPLOAD"
+while [ $i -ne 0 ]
+do
+ssh $USERMAN_LOGIN@$USERMAN_IP "${CMD}" > /dev/null
+STATUS=$?
+FUNC_DATE
+if [ $STATUS -ne 0 ]
+then
+let i=i-1
+echo "$DATE ssh no connect" >>$LOG
+sleep $CONNECT_INTERVAL
+else
+echo "$DATE ssh connect OK" >>$LOG
+touch $SSH_TRUE
+i=0
+fi
+
+done
 
 # version 2
 # wget https://github.com/mikbill/design/raw/master/radiuscash/radiuscash_2.sh
-
