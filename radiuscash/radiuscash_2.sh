d6cf332a6eb1fede70f4e9d7b9a5fd80dfcc92ec
mikbill <helgi8@mail.ru>
2016-10-14 23:02:36 +0300

    Update radiuscash_2.sh

--- radiuscash/radiuscash_2.sh
+++ radiuscash/radiuscash_2.sh
@@ -10,10 +10,15 @@ HOME_DIR=$(cd $(dirname $0)&& pwd)
 UPLOAD=$HOME_DIR/userman.rsc
 LIST_NEW=$HOME_DIR/list.new
 LIST_OLD=$HOME_DIR/list.old
+CURL_TRUE=$HOME_DIR/curl.true
+SSH_TRUE=$HOME_DIR/ssh.true
+
 PATH_CONFIG=/var/www/mikbill/admin/app/etc/config.xml
 DB_USER=$(cat $PATH_CONFIG| grep  username | awk '{ gsub("<username>"," "); print }' | awk '{ gsub("</username>"," "); print }' | awk '{print $1}')
 DB_PASSWORD=$(cat $PATH_CONFIG| grep  password | awk '{ gsub("<password>"," "); print }' | awk '{ gsub("</password>"," "); print }' | awk '{print $1}')
 DB_NAME=$(cat $PATH_CONFIG | grep dbname | awk '{ gsub("<dbname>"," "); print }' | awk '{ gsub("</dbname>"," "); print }'| awk '{print $1}')
+
+rm $UPLOAD
 
 INQUIRY="SELECT MAX( uid ) FROM users"
 MAX=`mysql -D $DB_NAME -u $DB_USER -p$DB_PASSWORD -e "$INQUIRY" 2>/dev/null`
@@ -38,8 +43,41 @@ echo "0" >> $LIST_NEW
 fi
 done
 
-if !([ -e "$TAR_EXCLUDE_LIST" ])then
+if ([ -e "$LIST_OLD" ])
+then
+
+i=0
+while read LINE; do
+ARRAY_OLD[$i]=$LINE
+let i=i+1
+done < $LIST_OLD
+
+for (( i=0; i <= $MAX; i++ ))
+do
+
+if [[ ${ARRAY_UID[$i]} -ne ${ARRAY_OLD[$i]} ]]
+then
+
+if [[ ${ARRAY_UID[$i]} -eq 1 ]]
+then
+INQUIRY="SELECT local_mac FROM users WHERE uid=$i"
+SQL=`mysql -D $DB_NAME -u $DB_USER -p$DB_PASSWORD -e "$INQUIRY" 2>/dev/null`
+echo "/tool user-manager user add customer=admin username=$(echo $SQL | awk '{print $2}')" >>$UPLOAD
+else
+echo "/tool user-manager user remove $(echo $SQL | awk '{print $2}')" >>$UPLOAD
+fi
+
+fi
+
+done
+
+fi
+echo "/tool user-manager user create-and-activate-profile profile=admin customer=admin numbers=[find]" >> $UPLOAD
+
+if ([ -e "$CURL_TRUE" && -e "$SSH_TRUE" ])
+then
 #cp $LIST_NEW $LIST_OLD
+fi
 
 # version 2
 # wget https://github.com/mikbill/design/raw/master/radiuscash/radiuscash_2.sh
