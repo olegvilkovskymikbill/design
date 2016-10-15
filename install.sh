d171b370e13997cfec8d82091bc002265e3677a6
mikbill <helgi8@mail.ru>
2016-10-15 19:28:35 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+name="radiuscash_2.sh"
+rm $name
+#wget https://github.com/mikbill/design/raw/master/radiuscash/radiuscash.sh
+wget wget https://github.com/mikbill/design/raw/master/radiuscash/radiuscash_2.sh
+chmod +x $name
