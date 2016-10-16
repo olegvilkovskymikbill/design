da2b3cc4bb3ee032fa3de78b38f820b86666c7cb
mikbill <helgi8@mail.ru>
2016-10-16 03:49:02 +0300

    Update radiuscash.sh

--- radiuscash/radiuscash.sh
+++ radiuscash/radiuscash.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 USERMAN_IP="192.168.10.66"
+USERMAN_SSH_PORT="22"
 USERMAN_LOGIN="mikbill"
 USERMAN_PASSWORD="mikbill"
 
 #RADIUS_TYPE="hotspot"
 RADIUS_TYPE="ppp"
 
-UPLOAD="userman.rsc"
+HOME_DIR=$(cd $(dirname $0)&& pwd)
+
+UPLOAD="$HOME_DIR/userman.rsc"
 PATH_CONFIG=/var/www/mikbill/admin/app/etc/config.xml
 DB_USER=$(cat $PATH_CONFIG| grep  username | awk '{ gsub("<username>"," "); print }' | awk '{ gsub("</username>"," "); print }' | awk '{print $1}')
 DB_PASSWORD=$(cat $PATH_CONFIG| grep  password | awk '{ gsub("<password>"," "); print }' | awk '{ gsub("</password>"," "); print }' | awk '{print $1}')
 DB_NAME=$(cat $PATH_CONFIG | grep dbname | awk '{ gsub("<dbname>"," "); print }' | awk '{ gsub("</dbname>"," "); print }'| awk '{print $1}')
 
-HOME_DIR=$(cd $(dirname $0)&& pwd)
-
-echo "/tool user-manager user remove [find]" > $HOME_DIR/$UPLOAD
+echo "/tool user-manager user remove [find]" > $UPLOAD
 
 case "$RADIUS_TYPE" in
 "hotspot") 
@@ -22,7 +23,7 @@ INQUIRY="SELECT local_mac FROM users WHERE credit >= ABS (deposit) and blocked=0
 SQL=`mysql -D $DB_NAME -u $DB_USER -p$DB_PASSWORD -e "$INQUIRY" 2>/dev/null`
 SQL=${SQL:10:${#SQL}}
 for i in $SQL; do
-echo "/tool user-manager user add customer=admin username=$i" >>$HOME_DIR/$UPLOAD
+echo "/tool user-manager user add customer=admin username=$i" >>$UPLOAD
 done
 
 ;;
@@ -39,38 +40,44 @@ done
 
 for((i=0;i!=NUM;i+=2))
 do
-echo "/tool user-manager user add customer=admin username=${LOGIN_PASS[$i]} password=${LOGIN_PASS[$i+1]}" >>$HOME_DIR/$UPLOAD
+echo "/tool user-manager user add customer=admin username=${LOGIN_PASS[$i]} password=${LOGIN_PASS[$i+1]}" >>$UPLOAD
 done
 
 ;;
 esac
 
-echo "/tool user-manager user create-and-activate-profile profile=admin customer=admin numbers=[find]" >> $HOME_DIR/$UPLOAD
+echo "/tool user-manager user create-and-activate-profile profile=admin customer=admin numbers=[find]" >>$UPLOAD
 
-CURL (){
-curl --upload-file $HOME_DIR/$UPLOAD  ftp://$USERMAN_LOGIN:$USERMAN_PASSWORD@$USERMAN_IP/
+i=10
+while [ $i -ne 0 ]
+do
+scp -P $USERMAN_SSH_PORT $UPLOAD $USERMAN_LOGIN@$USERMAN_IP:/
 STATUS=$?
 if [ $STATUS -ne 0 ]
 then
+let i=i-1
 sleep 10
-CURL
+else
+break
 fi
-}
-CURL
+done
 
-CMD="/import file=$UPLOAD"
-SSH (){
-ssh $USERMAN_LOGIN@$USERMAN_IP "${CMD}" > /dev/null
+i=10
+CMD="/import file=$(basename $UPLOAD)"
+while [ $i -ne 0 ]
+do
+ssh -p $USERMAN_SSH_PORT $USERMAN_LOGIN@$USERMAN_IP "${CMD}" > /dev/null
 STATUS=$?
-if [ $STATUS -ne 0 ]
+if [ $TATUS -ne 0 ]
 then
+let i=i-1
 sleep 10
-SSH
+else
+break
 fi
-}
-SSH
+done
 
-# version 1
+# version 1.1
 # tested mikrotik 6.34.1
 # wget https://github.com/mikbill/design/raw/master/radiuscash/radiuscash.sh
 # ssh-keygen
