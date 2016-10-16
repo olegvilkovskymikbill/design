efd59cc9bec4614804895d6788deeb45c74b5143
mikbill <helgi8@mail.ru>
2016-10-16 04:23:34 +0300

    Update radiuscash.sh

--- radiuscash/radiuscash.sh
+++ radiuscash/radiuscash.sh
@@ -57,7 +57,7 @@ for (( i=0;i!=10;i++ )); do
 
 scp -P $USERMAN_SSH_PORT $UPLOAD $USERMAN_LOGIN@$USERMAN_IP:/
 STATUS=$?
-if [ $STATUS -ne 0 ] then
+if [ $STATUS -ne 0 ]; then
 sleep 10
 else
 
@@ -65,7 +65,7 @@ CMD="/import file=$(basename $UPLOAD)"
 for (( i=0;i!=10;i++ )); do
 ssh -p $USERMAN_SSH_PORT $USERMAN_LOGIN@$USERMAN_IP "${CMD}" > /dev/null
 STATUS=$?
-if [ $TATUS -ne 0 ] then
+if [ $TATUS -ne 0 ]; then
 sleep 10
 fi
 
