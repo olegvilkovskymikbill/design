c952f0e2954e6e2f7331fa63c286fdf3a8a5a41e
mikbill <helgi8@mail.ru>
2016-10-20 16:47:37 +0300

    Update limits_update.sh

--- radiuscash/limits_update.sh
+++ radiuscash/limits_update.sh
@@ -3,14 +3,26 @@ HOME_DIR=$(cd $(dirname $0)&& pwd)
 source $HOME_DIR/radcash.conf
 
 INQUIRY="SELECT MAX( gid ) FROM packets"
-MAX_GID=`mysql -D $DB_NAME -u $DB_USER -p$DB_PASSWORD -e "$INQUIRY" 2>/dev/null`
+MAX_GID=`mysql -D $DB_NAME -u $DB_USER -p$DB_PASSWORD -e "$INQUIRY"`
 MAX_GID=${MAX_GID:11:${#MAX_GID}}
 
 INQUIRY="SELECT gid, speed_rate, speed_burst FROM packets"
 SQL=`mysql -D $DB_NAME -u $DB_USER -p$DB_PASSWORD -e "$INQUIRY" 2>/dev/null`
-SQL=${SQL:14:${#SQL}}
+SQL=${SQL:27:${#SQL}}
 
-for (( i=0; i <= $MAX_GID; i++ ))
+n=0
+for i in $SQL; do
+ARRAY_SQL[$n]=$i
+let n=n+1
+done
+
+echo "/tool user-manager profile" >$UPLOAD_LIMITS
+echo "limitation remove [find]" >>$UPLOAD_LIMITS
+echo "remove numbers=[find]" >>$UPLOAD_LIMITS
+
+for (( i=0; i <= $MAX_GID; i=i+3 ))
 do
-echo $SQL | awk '{print $2}
+echo "limitation add name=$i owner=admin rate-limit-rx=$i+1 rate-limit-tx=$i+2" >>$UPLOAD_LIMITS
+echo "add name=$i owner=admin" >>$UPLOAD_LIMITS
+echo "profile-limitation add profile=$i limitation=$i" >>$UPLOAD_LIMITS
 done
