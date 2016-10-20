f84e7374c91b7b3207492e3cff0bdcd266d29744
mikbill <helgi8@mail.ru>
2016-10-20 23:40:25 +0300

    Create radcash.sh

---
+++ radiuscash/radcash_limits/radcash.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+HOME_DIR=$(cd $(dirname $0)&& pwd)
+source $HOME_DIR/radcash.conf
+source $HOME_DIR/radcash.lib
+
+FUNC_MAX_UID
+
+echo "/tool user-manager user" >$UPLOAD
+echo "remove [find]" >>$UPLOAD
+
+UID_TO_SQL
+
+if [ "$RADIUS_HOTSPOT" -ne 0 ]
+then
+FUNC_HOTSPOT
+fi
+
+if [ "$RADIUS_PPP" -ne 0 ]
+then
+FUNC_PPP
+fi
+
+SSH_UPLOAD
