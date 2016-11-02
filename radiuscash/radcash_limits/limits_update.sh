62f424ce655105318daa022ddf32543e40c75a52
mikbill <helgi8@mail.ru>
2016-11-02 16:14:31 +0300

    Update limits_update.sh

--- radiuscash/radcash_limits/limits_update.sh
+++ radiuscash/radcash_limits/limits_update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 HOME_DIR=$(cd $(dirname $0)&& pwd)
 source $HOME_DIR/radcash.conf
+source $HOME_DIR/radcash.lib
 
 INQUIRY="SELECT MAX( gid ) FROM packets"
 MAX_GID=`mysql -D $DB_NAME -u $DB_USER -p$DB_PASSWORD -e "$INQUIRY"`
@@ -16,39 +17,15 @@ ARRAY_SQL[$n]=$i
 let n=n+1
 done
 
-echo "/tool user-manager profile" >$UPLOAD_LIMITS
-echo "limitation remove [find]" >>$UPLOAD_LIMITS
-echo "remove numbers=[find]" >>$UPLOAD_LIMITS
+echo "/tool user-manager profile" >$UPLOAD
+echo "limitation remove [find]" >>$UPLOAD
+echo "remove numbers=[find]" >>$UPLOAD
 
 for (( i=0; i < $MAX_GID; i=i+3 ))
 do
-echo "limitation add name=${ARRAY_SQL[$i]} owner=admin rate-limit-rx=${ARRAY_SQL[$i+1]}k rate-limit-tx=${ARRAY_SQL[$i+2]}k" >>$UPLOAD_LIMITS
-echo "add name=${ARRAY_SQL[$i]} owner=admin" >>$UPLOAD_LIMITS
-echo "profile-limitation add profile=${ARRAY_SQL[$i]} limitation=${ARRAY_SQL[$i]}" >>$UPLOAD_LIMITS
+echo "limitation add name=${ARRAY_SQL[$i]} owner=admin rate-limit-rx=${ARRAY_SQL[$i+1]}k rate-limit-tx=${ARRAY_SQL[$i+2]}k" >>$UPLOAD
+echo "add name=${ARRAY_SQL[$i]} owner=admin" >>$UPLOAD
+echo "profile-limitation add profile=${ARRAY_SQL[$i]} limitation=${ARRAY_SQL[$i]}" >>$UPLOAD
 done
 
-#SSH
-FUNC_SSH (){
-for (( i=0;i!=$CONNECT_SUM;i++ )); do
-
-scp -P $USERMAN_SSH_PORT $UPLOAD_LIMITS $USERMAN_LOGIN@$USERMAN_IP:/
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-sleep $CONNECT_INTERVAL
-else
-
-CMD="/import file=$(basename $UPLOAD_LIMITS)"
-for (( i=0;i!=$CONNECT_SUM;i++ )); do
-ssh -p $USERMAN_SSH_PORT $USERMAN_LOGIN@$USERMAN_IP "${CMD}" > /dev/null
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-sleep $CONNECT_INTERVAL
-fi
-
-done
-break
-fi
-
-done
-}
-FUNC_SSH
+SSH_UPLOAD
