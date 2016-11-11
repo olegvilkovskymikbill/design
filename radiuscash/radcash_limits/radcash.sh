ca218f49f2df5219ef9f0b8deb2f732afe18281c
mikbill <helgi8@mail.ru>
2016-11-11 20:16:43 +0300

    Update radcash.sh

--- radiuscash/radcash_limits/radcash.sh
+++ radiuscash/radcash_limits/radcash.sh
@@ -5,12 +5,14 @@ source $HOME_DIR/radcash.lib
 
 FUNC_MAX_UID
 
+echo >$UPLOAD
+
 if [ "$LOG_REMOVE" -ne 0 ]
 then
 echo "/tool user-manager log remove numbers=[find]" >>$UPLOAD
 fi
 
-echo "/tool user-manager user" >$UPLOAD
+echo "/tool user-manager user" >>$UPLOAD
 echo "remove [find]" >>$UPLOAD
 
 UID_TO_USERS
