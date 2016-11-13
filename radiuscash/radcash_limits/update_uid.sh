3779138e156e82ace4ed4f6ad890902413c783de
mikbill <helgi8@mail.ru>
2016-11-13 18:02:21 +0300

    Update update_uid.sh

--- radiuscash/radcash_limits/update_uid.sh
+++ radiuscash/radcash_limits/update_uid.sh
@@ -3,15 +3,15 @@ HOME_DIR=$(cd $(dirname $0)&& pwd)
 source $HOME_DIR/radcash.conf
 source $HOME_DIR/radcash.lib
 
-echo >$UPLOAD
-echo "/tool user-manager user" >>$UPLOAD
-
 ID=$1
 
 FUNC_CHECK_ID
 
 if [ "$CHECK_ID" != "" ]
 then
+
+echo >$UPLOAD
+echo "/tool user-manager user" >>$UPLOAD
 
 if [ "$RADIUS_HOTSPOT" -ne 0 ]
 then
