4e5cabe2435c41f0c5f12eca8c4a0b294f6001ef
mikbill <helgi8@mail.ru>
2016-11-13 17:23:22 +0300

    Update update_uid.sh

--- radiuscash/radcash_limits/update_uid.sh
+++ radiuscash/radcash_limits/update_uid.sh
@@ -8,14 +8,21 @@ echo "/tool user-manager user" >>$UPLOAD
 
 ID=$1
 
+FUNC_CHECK_ID
+
+if [ "$CHECK_ID" == $ID ]
+then
+
 if [ "$RADIUS_HOTSPOT" -ne 0 ]
-then                                                                     
-FUNC_HOTSPOT_ID                                                          
-fi                                                                       
-                                                                         
-if [ "$RADIUS_PPP" -ne 0 ]                                               
-then                                                                     
-FUNC_PPP_ID                                                              
+then
+FUNC_HOTSPOT_ID
+fi
+
+if [ "$RADIUS_PPP" -ne 0 ]
+then
+FUNC_PPP_ID
 fi
 
 SSH_UPLOAD
+
+fi
