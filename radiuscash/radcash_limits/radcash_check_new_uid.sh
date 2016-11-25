340b5a71f9a6444d906806cf75019cf98411ebbf
mikbill <helgi8@mail.ru>
2016-11-25 18:42:58 +0300

    Update radcash_check_new_uid.sh

--- radiuscash/radcash_limits/radcash_check_new_uid.sh
+++ radiuscash/radcash_limits/radcash_check_new_uid.sh
@@ -4,5 +4,32 @@ source $HOME_DIR/radcash.conf
 source $HOME_DIR/radcash.lib
 
 FUNC_MAX_UID
+MAX_UID_OLD=$(cat $MAX_UID_FILE)
+
+echo >$UPLOAD
+let "MAX_UID_OLD=MAX_UID_OLD+1"
+for (( i=$MAX_UID_OLD; i <= $MAX_UID; i++ ))
+do                                                                  
+ID=$i                                                               
+FUNC_CHECK_ID                                                       
+                                                                    
+if [ "$CHECK_ID" != "" ]                                            
+then                                                                
+echo "/tool user-manager user" >>$UPLOAD                            
+
+if [ "$RADIUS_HOTSPOT" -ne 0 ]
+then
+FUNC_HOTSPOT_ID
+fi
+
+if [ "$RADIUS_PPP" -ne 0 ]
+then
+FUNC_PPP_ID
+fi
+fi
+
+done
+
+SSH_UPLOAD
 
 echo $MAX_UID >$MAX_UID_FILE
