77435b9d66db3aa43053a36eb910619bc756d61d
mikbill <helgi8@mail.ru>
2016-12-21 07:30:52 +0300

    Update backupForUpdate.sh

--- backupForUpdate/backupForUpdate.sh
+++ backupForUpdate/backupForUpdate.sh
@@ -12,6 +12,8 @@ DB_Password=""
 DB_Name="mikbill"
 
 Date=`date +%Y-%m-%d_%Hh%Mm`
+GREEN='\033[0;32m'
+NC='\033[0m'
 
 MySQL_dump (){
 if [ "$DB_User" = "" ]
@@ -28,17 +30,23 @@ mkdir -p $Path_backup
 
 if [ "$Backup_routines" -ne 0 ]
 then
-mysqldump --single-transaction --routines --extended-insert -u $DB_User -p$DB_Password mikbill | gzip > $Path_backup/"$Date"_backup_mikbill_DB_routines.sql.gz
+File=$Path_backup/"$Date"_backup_mikbill_DB_routines.sql.gz
+mysqldump --single-transaction --routines --extended-insert -u $DB_User -p$DB_Password mikbill | gzip > $File
 else
-mysqldump --single-transaction -u $DB_User -p$DB_Password mikbill | gzip > $Path_backup"$Date"_mikbill_DB.sql.gz
+File=$Path_backup"$Date"_mikbill_DB.sql.gz
+mysqldump --single-transaction -u $DB_User -p$DB_Password mikbill | gzip > $File
 fi
+
+echo -e " ${GREEN}MySQL dump: $File Size: ls -lh $File{NC}"
 }
 
 Mikbill_files_backup (){
 tar -czf $Path_backup"$Date"_mikbill_files.tar.gz 
 }
 
-echo "Free space on $Path_backup: $(df -h $Path_backup)"
+echo -e " ${GREEN}Free space on $Path_backup:{NC}"
+echo "$(df -h $Path_backup)"
+
 echo -n "[1] - MySQL dump, [2] - MySQL dump + mikbill files:"
 read NUM
 case "$NUM" in
@@ -50,3 +58,5 @@ case "$NUM" in
   Mikbill_files_backup
   ;;
 esac
+
+
