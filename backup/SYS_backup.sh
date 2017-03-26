41c2a759b3c416206eadd74a9ceaae05d937177d
mikbill <helgi8@mail.ru>
2017-03-26 13:36:30 +0300

    Update SYS_backup.sh

--- backup/SYS_backup.sh
+++ backup/SYS_backup.sh
@@ -218,6 +218,11 @@ if [ "$BACKUP_TO_OWNCLOUD" -ne 0 ]
 then
   owncloudcmd -u $Owncloud_login -p $Owncloud_pass $PACH_FOR_BACKUP_TO_DISK $Owncloud_address
 fi
+# DropBox -------------------------------------
+if [ "$BACKUP_TO_DROPBOX" -ne 0 ]
+then
+  ~/dropbox.py start >>$LOG
+fi
 #----------------------------------------------
 FUNC_TAR_DIFF()
 {
