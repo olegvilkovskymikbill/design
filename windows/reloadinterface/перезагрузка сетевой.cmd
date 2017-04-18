110648220df0f6a83bde183a55e76a095ed2e127
mikbill <helgi8@mail.ru>
2017-04-18 19:38:43 +0300

    Update перезагрузка сетевой.cmd

--- windows/reloadinterface/перезагрузка сетевой.cmd
+++ windows/reloadinterface/перезагрузка сетевой.cmd
@@ -1,27 +1,25 @@
 echo off
 chcp 1251
 
-echo Ńęđčďň ń đŕńřčđĺíčĺě *.cmd çŕďóńňčňü ń ďđŕâŕěč ŕäěčíčńňđŕňîđŕ
+echo Скрипт с расширением *.cmd запустить с правами администратора
 
-echo Ěĺí˙ĺě "Ethernet" íŕ íŕçâŕíčĺ ńâîĺé ńĺňĺâîé ęŕđňű 
+echo Меняем "Ethernet" на название своей сетевой карты 
 set network_interface=Ethernet
 
-echo timeout - âđĺě˙ îćčäŕíč˙ â âűęëţ÷ĺííîě ńîńňî˙íčč číňĺđôĺéńŕ
+echo timeout - время ожидания в выключенном состоянии интерфейса
 set timeout=10
 
-echo dst_address - ďđîâĺđ˙ĺěűé IP/Äîěĺí
+echo dst_address - проверяемый IP/Домен
 set dst_address=8.8.8.8
 
-echo num_packet - ęîëč÷ĺńňâî îňďđŕâë˙ĺěűő ďŕęĺňîâ ęîěŕíäîé ping
+echo num_packet - количество отправляемых пакетов командой ping
 set num_packet=5
 
 :start
-echo Âűęëţ÷ĺíčĺ ńĺňĺâîé ęŕđňű %network_interface%
+echo Выключение сетевой карты %network_interface%
 netsh interface set interface name=%network_interface% admin=disabled
-
- 
 TIMEOUT /T %timeout% /NOBREAK
-echo Âęëţ÷ĺíčĺ ńĺňĺâîé ęŕđňű %network_interface%
+echo Включение сетевой карты %network_interface%
 netsh interface set interface name=%network_interface% admin=enabled
 ping %dst_address% -n %num_packet%
 goto start
