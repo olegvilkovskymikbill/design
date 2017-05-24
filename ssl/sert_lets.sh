183c39565e7567891a9b0e9da4f9a2d1b9be6ab5
mikbill <helgi8@mail.ru>
2017-05-25 01:55:08 +0300

    Create sert_lets.sh

---
+++ ssl/sert_lets.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Админка
+admin=
+# Личный кабинет
+stat=
+# Карты
+map=
+
+path_mikbill=/var/www/mikbill/
+path_ssl=/etc/nginx/conf.d/ssl
+
+yum -y install nc wget
+wget -O - https://get.acme.sh | sh
+
+function add_domain(){
+if ! [[ -z $1 ]]; then
+/root/.acme.sh/acme.sh --issue -d $1 -w $path_mikbill/$2
+cp ~/.acme.sh/$1/fullchain.cer $path_ssl/$1.cer
+cp ~/.acme.sh/$1/$1.key $path_ssl/$1.key
+echo "Add to conf $1:"
+echo -e "\033[0;35mssl_certificate $path_ssl/$1.cer; \033[0m"
+echo -e "\033[0;35mssl_certificate_key $path_ssl/$1.key; \033[0m"
+echo "Add to cron $1"
+echo -e "\033[0;35m00 05 * * * root /root/.acme.sh/acme.sh --renew -d $1  \033[0m"
+fi
+}
+
+add_domain $admin admin
+add_domain $stat stat
+add_domain $map map
