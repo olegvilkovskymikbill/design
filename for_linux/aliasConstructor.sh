db7763ad2d1fedda4575056ae01adb2e0d1a6c6d
mikbill <helgi8@mail.ru>
2017-05-25 19:18:22 +0300

    Update aliasConstructor.sh

--- for_linux/aliasConstructor.sh
+++ for_linux/aliasConstructor.sh
@@ -1,1 +1,22 @@
 
+!/bin/bash
+
+ipaddr=213.174.7.
+HostMin=65
+HostMax=94
+filename=alias.sh
+echo>$filename
+
+i=$HostMin
+n=0
+echo "#!/bin/bash">$filename
+for ((i=$HostMin; i<=$HostMax; i++))
+do
+echo "ifconfig lo:$n $ipaddr$i  netmask 255.255.255.255" >>$filename
+let "n=n+1"
+done
+chmod +x $filename
+
+echo "Add to /etc/rc.local:"
+echo "#aliases for NAT"
+echo "/etc/network/$filename"
