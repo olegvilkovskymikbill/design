f5f362f0a3851992f1ab4af4b69684d1c4662c2e
mikbill <helgi8@mail.ru>
2017-05-28 12:35:36 +0300

    Update SYS_backup.sh

--- backup/SYS_backup.sh
+++ backup/SYS_backup.sh
@@ -66,7 +66,7 @@ fi
 Func_copy_to_Gdive()
 {
 if [ "$Backup_to_Gdrive" -ne 0 ];then
-gdrive --access-token $Gdrive_access-token upload $PACH_FOR_BACKUP_TO_DISK/$FILENAME
+gdrive --access-token $Gdrive_access_token upload $PACH_FOR_BACKUP_TO_DISK/$FILENAME
 fi
 }
 
