10d19b581bf9c91b397b2a562f9bd6063d02a95f
mikbill <helgi8@mail.ru>
2017-06-07 21:30:21 +0300

    Update ssh_key_authorized.sh

--- accel/accel-cache/ssh_key_authorized.sh
+++ accel/accel-cache/ssh_key_authorized.sh
@@ -4,7 +4,5 @@ REMOTE_HOST=10.10.10.10
 PORT=2280
 USER=bill
 ssh-keygen -t rsa
-echo $HOME
 ssh-copy-id -i $HOME/.ssh/id_rsa.pub "-p $PORT $USER@$REMOTE_HOST"
-echo $USER
 ssh $USER@$REMOTE_HOST -p $PORT
