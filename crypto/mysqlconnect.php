569e446f410dc388a5c07184c6fcbe10152f7330
mikbill <helgi8@mail.ru>
2017-08-24 16:27:05 +0300

    Update mysqlconnect.php

--- crypto/mysqlconnect.php
+++ crypto/mysqlconnect.php
@@ -6,12 +6,18 @@ Class Database{
 
   public function __construct()
   {
-  
+    $this=>connect();
   }
 
   private function connect()
   {
+    $config=require_once 'config.php';
+    
+    $dsn='mysql:host='.$config['host'].';dbname='.$config['db_name'].';charset='.$config['charset'];
+    
     this->link=new PDO($dsn, $username, $password);
+    
+    return $this;
   }
   
   public function execute($sql)
