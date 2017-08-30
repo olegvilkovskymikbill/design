beacc4d0a880fa1d7197a07f8e9302eb0ed41cc1
mikbill <helgi8@mail.ru>
2017-08-30 12:16:21 +0300

    Update radcash_uploader.sh

--- accel/accel-cache/accel_cash_v2/radcash_uploader.sh
+++ accel/accel-cache/accel_cash_v2/radcash_uploader.sh
@@ -1,5 +1,16 @@
                                                                                                                                                                                                                153/153               100%#!/bin/bash
 upload_server=https://mydomain.com
 file=users_5zoMuYNFkS6T
+
+# Указываем минимальный размер скачиваемого файла
+# Нужно для проверки, чтобы при проблемах закачки файл не перезаписался
+size=349603
+
 HOME_DIR=$(cd $(dirname $0)&& pwd)
-wget -O $HOME_DIR/users $upload_server/$file
+wget -O $HOME_DIR/$file $upload_server/$file --no-check-certificate
+
+size_tmp=$(stat $file -c %s)
+#echo $size_tmp
+if [ "$size_tmp" -gt "$size" ]; then
+$(cp -f $file users)
+fi
