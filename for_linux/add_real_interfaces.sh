3104e1b69a00aa7e8053efdc5671cceb1bb29cb0
mikbill <helgi8@mail.ru>
2018-02-16 10:50:38 +0300

    Update add_real_interfaces.sh

--- for_linux/add_real_interfaces.sh
+++ for_linux/add_real_interfaces.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-
 file=interfaces
+file_2=interfaces_no_reboot.sh
 
 ip=213.174.7.
 min=65
 max=94
-
 n=1
-for ((i=$min; i<=$max; i++))
-do
-echo >>$file
 
+echo "#!/bin/bash" >$file_2          
+chmod +x $file_2                     
+                                     
+for ((i=$min; i<=$max; i++))         
+do                                   
+echo >>$file                         
+                                     
 echo "auto lo:$n">>$file
 echo "   iface lo:$n inet static">>$file
 echo "   address $ip$i">>$file
 echo "   netmask 255.255.255.255">>$file
 
-let "n=n+1"
+echo "ifconfig lo:$n $ip$i netmask 255.255.255.255 up" >>$file_2
 
+let "n=n+1"
 done
