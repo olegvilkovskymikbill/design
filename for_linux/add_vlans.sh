823e2f70322ffa6063635906a0c3d2c5fe3f4a3b
mikbill <helgi8@mail.ru>
2018-02-22 10:39:03 +0300

    Update add_vlans.sh

--- for_linux/add_vlans.sh
+++ for_linux/add_vlans.sh
@@ -5,16 +5,16 @@ file_2=vlans_no_reboot.sh
 
 vlanMin=3500
 vlanMax=3700
-first=eth4.
+first=eth4
 
 echo "#!/bin/bash" >$file_2
-chmod +x $file_2                      
-                                          
-for ((i=$vlanMin; i<=$vlanMax; i++))      
-do                                        
-echo >>$file                              
-echo "auto $first$i">>$file
-echo "  iface $first$i inet manual">>$file
+chmod +x $file_2
+
+for ((i=$vlanMin; i<=$vlanMax; i++))
+do
+echo >>$file
+echo "auto $first.$i">>$file
+echo "  iface $first.$i inet manual">>$file
 echo '  post-up ifconfig $IFACE up'>>$file
 echo '  pre-down ifconfig $IFACE down'>>$file
 
