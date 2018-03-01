0c8e3826582ddebe36fbfb167c609c7af009a26f
mikbill <helgi8@mail.ru>
2018-03-01 17:34:46 +0300

    Update add_real_interfaces.sh

--- for_linux/add_real_interfaces.sh
+++ for_linux/add_real_interfaces.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-
 file=interfaces
-file_2=vlans_no_reboot.sh
+file_2=interfaces_no_reboot.sh
 
-vlanMin=3500
-vlanMax=3700
-first=eth4.
+ip=93.171.177.
+min=1
+max=62
+n=31
 
 echo "#!/bin/bash" >$file_2
-chmod +x $file_2                      
-                                          
-for ((i=$vlanMin; i<=$vlanMax; i++))      
-do                                        
-echo >>$file                              
-echo "auto $first$i">>$file
-echo "  iface $first$i inet manual">>$file
-echo '  post-up ifconfig $IFACE up'>>$file
-echo '  pre-down ifconfig $IFACE down'>>$file
+chmod +x $file_2
 
-echo "/sbin/vconfig add $first $i" >>$file_2
-echo "/sbin/ifconfig $first.$i up" >>$file_2
+for ((i=$min; i<=$max; i++))
+do
+echo >>$file
 
+echo "auto lo:$n">>$file
+echo "   iface lo:$n inet static">>$file
+echo "   address $ip$i">>$file
+echo "   netmask 255.255.255.255">>$file
+
+echo "ifconfig lo:$n $ip$i netmask 255.255.255.255 up" >>$file_2
+
+let "n=n+1"
 done
