d72e96399d948e9e44ccd8401d3154196cf51c0e
mikbill <helgi8@mail.ru>
2018-09-18 14:35:26 +0300

    Update adminuser.sh

--- mikbill/adminuser/adminuser.sh
+++ mikbill/adminuser/adminuser.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-PATH_MIKBILL=/var/www/mikbill/
+PATH_MIKBILL=/var/www/mikbill/admin/
+DB_NAME=mikbill
 DB_USER=$(cat $PATH_MIKBILL'app/etc/config.xml'| grep  username | awk '{ gsub("<username>"," "); print }' | awk '{ gsub("</username>"," "); print }' | awk '{print $1}')
 DB_PASSWORD=$(cat $PATH_MIKBILL'app/etc/config.xml'| grep  password | awk '{ gsub("<password>"," "); print }' | awk '{ gsub("</password>"," "); print }' | awk '{print $1}')
+SQL=`mysql -D $DB_NAME -u $DB_USER -p$DB_PASSWORD -e "select login, pass from stuff_personal;"`
+echo $SQL
 
-SQL=`mysql -D $DB_NAME -u $DB_USER -p$DB_PASSWORD -e "USE MIKBILL;"
-SQL=`mysql -D $DB_NAME -u $DB_USER -p$DB_PASSWORD -e "select * from stuff_personal;"
-
-
-
-#mysql -u$DB_USER -p$DB_PASSWORD
-#USE mikbill;
-#select * from stuff_personal;
