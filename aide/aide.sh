9dbdd36dbab51ed6bbb50b123bdcdc749185f096
mikbill <helgi8@mail.ru>
2018-09-21 12:29:01 +0300

    Update aide.sh

--- aide/aide.sh
+++ aide/aide.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-server_name="[] ";
-TOKEN=1xoxb-78422713744-425720419425-U3Men2QcEWZgFrXBTtTAUuEH
-CHANNEL=1events
-USERNAME=1events_bot
+server_name="";
+TOKEN=xoxb-78422713744-425720419425-U3Men2QcEWZgFrXBTtTAUuEH1
+CHANNEL=events
+USERNAME=events_bot
 
 DATE=`date +%Y-%m-%d`
 logdir=/var/log/aide
@@ -15,5 +15,10 @@ aide --update
 cp $aidedir/aide.db.gz     $logdir/aide.db.gz-$DATE
 mv $aidedir/aide.db.new.gz $aidedir/aide.db.gz
 
+
+size=$(cat $logdir/$REPORT |wc -l)
+if [ "$size" != "0" ]
+then
 MESSAGE="$server_name, $(< $logdir/$REPORT)"
 curl -s -X POST https://slack.com/api/chat.postMessage --data "token=$TOKEN&channel=$CHANNEL&text=$MESSAGE&username=$USERNAME&mrkdwn=true" >/dev/null
+fi                   
