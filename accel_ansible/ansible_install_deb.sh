cc061572899dafffa2173b5984cf4d4ead9956c6
mikbill <helgi8@mail.ru>
2019-02-27 22:44:35 +0300

    Create ansible_install_deb.sh

---
+++ accel_ansible/ansible_install_deb.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+apt-get update
+apt-get install ansible -y
