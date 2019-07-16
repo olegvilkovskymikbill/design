1dbc8811a22acb2ef0dc585aa41fc30226916f45
Oleg Vilkovsky <helgi8@mail.ru>
2019-07-16 21:47:03 +0300

    Create check_free_space.sh

---
+++ for_linux/check_free_space/check_free_space.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+FILE1=du1.txt
+FILE2=du2.txt
+while read LINE1 ;do
+  array1=($(echo $LINE1 | tr " " "\n"))
+  key1=${array1[1]}
+  while read LINE2 ;do
+    array2=($(echo $LINE2 | tr " " "\n"))
+    key2=${array2[1]}
+    if [ "$key1" = "$key2" ] ;then
+      value1=${array1[0]}
+      value2=${array2[0]}
+      let "diff=value2-value1"
+      if [ "$diff" != "0" ] ;then
+        echo "$key1 $diff"
+#      else
+#        echo "$key1 OK"
+      fi
+      break
+    fi
+  done < $FILE2
+done < $FILE1
